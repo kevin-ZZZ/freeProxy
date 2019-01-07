4e4b9b3fed06eab2a4c89b457e3c7145dc7a50ce
张旋华 <2358069852@qq.com>
2019-01-07 20:10:14 +0800

    明天安装messagebus

--- freeProxy/settings.py
+++ freeProxy/settings.py
@@ -45,14 +45,17 @@ DOWNLOAD_DELAY = 0.5
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    'freeProxy.middlewares.FreeproxySpiderMiddleware': 543,
-# }
+SPIDER_MIDDLEWARES = {
+    'frontera.contrib.scrapy.middlewares.schedulers.SchedulerSpiderMiddleware': 1000,
+    # 'freeProxy.middlewares.FreeproxySpiderMiddleware': 543,
+
+}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'freeProxy.middlewares.RandomUserAgentMiddleware': 400,
+    'frontera.contrib.scrapy.middlewares.schedulers.SchedulerDownloaderMiddleware': 1000,
     # 'freeProxy.middlewares.FreeproxyDownloaderMiddleware': 543,
 }
 
@@ -65,7 +68,7 @@ DOWNLOADER_MIDDLEWARES = {
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'freeProxy.pipelines.JsonWriterPipeline': 300,
+    'freeProxy.pipelines.JsonWriterPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -95,3 +98,34 @@ USER_AGENT_LIST = [
     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
     "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
 ]
+
+SCHEDULER = 'frontera.contrib.scrapy.schedulers.frontier.FronteraScheduler'
+
+FRONTERA_SETTINGS = 'freeProxy.frontera.settings'
+
+
+HTTPCACHE_ENABLED = False
+REDIRECT_ENABLED = True
+COOKIES_ENABLED = False
+DOWNLOAD_TIMEOUT = 240
+RETRY_ENABLED = False
+DOWNLOAD_MAXSIZE = 1*1024*1024
+
+# auto throttling
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_MAX_DELAY = 3.0
+AUTOTHROTTLE_START_DELAY = 0.25
+RANDOMIZE_DOWNLOAD_DELAY = False
+
+# concurrency
+CONCURRENT_REQUESTS = 64
+CONCURRENT_REQUESTS_PER_DOMAIN = 10
+DOWNLOAD_DELAY = 0.0
+
+LOG_LEVEL = 'INFO'
+
+REACTOR_THREADPOOL_MAXSIZE = 32
+DNS_TIMEOUT = 180
+FRONTERA_SETTINGS = 'config.spider'
+HTTPERROR_ALLOW_ALL = True
