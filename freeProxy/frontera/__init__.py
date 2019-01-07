7c20dbf3d0929a5f5f563a5dc3146be773244099
张旋华 <2358069852@qq.com>
2019-01-07 20:08:02 +0800

    明天安装messagebus

---
+++ freeProxy/frontera/__init__.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+# -*- coding: utf-8 -*-
+# @Time    : 1/7/19 4:41 PM
+# @Author  : Kevin
+# @Site    : 
+# @File    : __init__.py.py
+# @Descri  : 
+# @Software: pycharm
---
+++ freeProxy/frontera/settings.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+# -*- coding: utf-8 -*-
+# @Time    : 1/7/19 4:41 PM
+# @Author  : Kevin
+# @Site    : 
+# @File    : setting.py
+# @Descri  : 
+# @Software: pycharm
